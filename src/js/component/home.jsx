72a6ead049637ae7b63d4833079375dfb70e09ca
mauriciio89 <maugalvez89@gmail.com>
2022-09-20 02:27:27 +0000

    react fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [toDo, setToDo] = useState([]);
+  const [tarea, setTarea] = useState([]);
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/mauricio89", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((todoList) => {
+        console.log(todoList);
+        setToDo(todoList);
+      });
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setToDo((prev) => [...prev, tarea]);
+    setTarea("");
+
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/mauricio89", {
+      method: "PUT",
+      body: JSON.stringify(toDo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch((error) => {
+        //manejo de errores
+        console.log(error);
+		
+      });
+  };
+  console.log(toDo);
+  const elementDellete = (dIndex) => {
+    setToDo(toDo.filter((e, i) => i != dIndex));
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="text-center">ToDo's</h1>
+        <form onSubmit={onSubmit}>
+          <div className="input-group d-flex justify-content-center">
+            <input
+              value={tarea}
+              placeholder="agrega tus tareas"
+              className="border-2 text-muted fs-4 container-fluid"
+              type="text"
+              onChange={(e) => setTarea(e.target.value)}
+            />
+          </div>
+          <div>
+            {toDo.map((element, dIndex) => {
+              return (
+                <div
+                  key={dIndex}
+                  className="d-flex justify-content-between my-3 col-4 mx-auto"
+                >
+                  <p>{element.label}</p>
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => elementDellete(dIndex)}
+                  >
+                    borrar
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
